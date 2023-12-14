5d03e38141059a3076370e3a064d4ac70d16bb95
Meniou56 <manu270687@gmail.com>
2023-12-14 15:03:50 +0100

    Fonction de tri des scores terminé dans la page scores !

--- scripts/scriptScores.js
+++ scripts/scriptScores.js
@@ -1,5 +1,6 @@
-// Attention un bug à la maj du tableau !
-
+/************************************
+* CHARGEMENT DES SCORES ENREGISTRES *
+*************************************/
 // Fonction de chargement des scores JSON
 async function chargerScores() {
     const data = await fetch("http://localhost:3000/api/scores")
@@ -13,17 +14,90 @@ async function initialiserTableau(listeChronoOrdonne) {
     creerTableauScores(scores, listeChronoOrdonne)
 }
 
+/***************************
+* TRI ET AFFICHAGE TABLEAU *
+****************************/
+
+/* Fonction de choix du mode de tri */
+function choixTriScores(){
+
+    clickTriScores = document.querySelector("#triScores")
+    clickTriScores.addEventListener("click", (event) => {
+
+        /* Si ordonné par dates, on regarde dans quel sens et on réinitlialise le tableau */
+        if(clickTriScores.value==="anteChrono"){
+            listeChronoOrdonne=false
+            initialiserTableau(listeChronoOrdonne)
+        }else if(clickTriScores.value==="chrono"){
+            listeChronoOrdonne=true
+            initialiserTableau(listeChronoOrdonne)
+        }
+
+        /*Si ordonné par Scores */
+        else if(clickTriScores.value==="highScores"){
+            let triCoupInverse = true
+            triScoresCoup(triCoupInverse)}
+        else if(clickTriScores.value==="lowScores"){
+            let triCoupInverse = false
+            triScoresCoup(triCoupInverse)
+        }
+
+        /* Si ordonné par ordre alphabétique */
+        else if(clickTriScores.value==="alphabetique"){
+            let triAlphabetiqueInverse = -1
+            triScoresUser(triAlphabetiqueInverse)
+        }
+        else if(clickTriScores.value==="anteAlphabetique"){
+            let triAlphabetiqueInverse = 1
+            triScoresUser(triAlphabetiqueInverse)
+        }
+
+    })
+}
+
+/* Fonction de tri des scores par scores (coups) */
+async function triScoresCoup(triCoupInverse){
+    let scores = await chargerScores()
+    if(triCoupInverse){
+        scores.sort(function (a, b){return b.coup - a.coup})
+    }else{
+        scores.sort(function (a, b){return a.coup - b.coup})
+    }
+
+    // On réordonne correctement & on crée avec les nouvelles données
+    listeChronoOrdonne=true
+    creerTableauScores(scores)
+}
+
+/* Fonction de tri des scores par nom (user) */
+async function triScoresUser(triAlphabetiqueInverse){
+    let scores = await chargerScores()
+
+    scores.sort(function (a, b){
+        if(a.user.toLowerCase() < b.user.toLowerCase()){return triAlphabetiqueInverse}
+        if(a.user.toLowerCase() > b.user.toLowerCase()){return -triAlphabetiqueInverse}
+        return 0
+    })
+
+    // On réordonne correctement & on crée avec les nouvelles données
+    listeChronoOrdonne=true
+    creerTableauScores(scores)
+}
+
 // Fonction Création du tableau des scores
 function creerTableauScores(scores, listeChronoOrdonne) {
 
     // On récupère l'emplacement du tableau dans le html
     let tableauBody = document.getElementById("tableauBody")
 
+    /*On commence par effacer le tableau */
+    eraseTableauScores()
+
     // Boucle de creation des lignes (en fonction du nombre d'entrées dans scores)
     // On commence par définir le sens de la boucle suivant si ordonné ou non
-    if(listeChronoOrdonne){startBoucle=0}else{startBoucle=scores.length-1}
-    if(listeChronoOrdonne){pasBoucle=+1}else{pasBoucle=-1}
-    if(listeChronoOrdonne){endBoucle=scores.length}else{endBoucle=-1}
+    if(!listeChronoOrdonne){startBoucle=0}else{startBoucle=scores.length-1}
+    if(!listeChronoOrdonne){pasBoucle=+1}else{pasBoucle=-1}
+    if(!listeChronoOrdonne){endBoucle=scores.length}else{endBoucle=-1}
 
     for (iRow=startBoucle; iRow!=endBoucle; iRow=iRow+pasBoucle) {
 
@@ -63,50 +137,9 @@ function eraseTableauScores(){
     }
 }
 
-/* Fonction de choix du mode de tri */
-function choixTriScores(){
-    clickTriScores = document.querySelector("#triScores")
-    clickTriScores.addEventListener("click", (event) => {
-
-        /*On commence par effacer le tableau */
-        eraseTableauScores()
-
-        /* Si ordonné par dates, on regarde dans quel sens et on réinitlialise le tableau */
-        if(clickTriScores.value==="anteChrono"){
-            listeChronoOrdonne=false
-            initialiserTableau(listeChronoOrdonne)
-        }else if(clickTriScores.value==="chrono"){
-            listeChronoOrdonne=true
-            initialiserTableau(listeChronoOrdonne)
-        }
-
-        /*Si ordonné par Scores */
-        else if(clickTriScores.value==="highScores"){
-            let triCoupInverse = true
-            triScoresCoup(triCoupInverse)}
-        else if(clickTriScores.value==="lowScores"){
-            let triCoupInverse = false
-            triScoresCoup(triCoupInverse)
-        }
-    })
-}
-
-/* Fonction de tri des scores */
-async function triScoresCoup(triCoupInverse){
-    let scores = await chargerScores()
-    if(triCoupInverse){
-        scores.sort(function (a, b){return a.coup - b.coup})
-    }else{
-        scores.sort(function (a, b){return b.coup - a.coup})
-    }
-
-    // On crée avec les nouvelles données
-    creerTableauScores(scores)
-}
-
 //LANCEMENT ET APPELS
 //Variable globale
-let listeChronoOrdonne=false
+let listeChronoOrdonne=true
 
 //Appel de la fonction d'initialisation du tableau
 initialiserTableau(listeChronoOrdonne)
