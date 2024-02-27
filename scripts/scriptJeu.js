da59ddf575bbbfb97b6358b304670d82ce2f765b
Meniou56 <manu270687@gmail.com>
2024-02-27 13:32:19 +0100

    re-correction de la valeur fullCol max à 6

--- scripts/scriptJeu.js
+++ scripts/scriptJeu.js
@@ -404,7 +404,7 @@ function detectionAlignement() {
                 
                 //Regarder pour chaque colonne si la dernière cellule est pleine
                 if(tablJeu[0][iCol]!==""){fullCol++}
-                if(fullCol===7){
+                if(fullCol===6){
                     messageWithButton("Égalité !")
                 }
             }
